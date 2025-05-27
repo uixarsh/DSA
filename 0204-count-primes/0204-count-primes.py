1776ed849bd52d35c84d669b9ac9648b6c33ec3c
Arsh Singhal <130887211+uixarsh@users.noreply.github.com>
2025-05-27 21:50:59 +0530

    Time: 0 ms (100%), Space: 106.6 MB (8.07%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -1,19 +1,22 @@
+max_n = int(5 * 1e6)
+
+primes = [True for _ in range(max_n + 1)]
+primes[0], primes[1] = False, False
+count_primes = [0 for _ in range(max_n + 1)]
+
+count = 0
+for i in range(2, max_n + 1):
+    if primes[i]:
+        count += 1
+        
+        for j in range(i * i, max_n + 1, i):
+            primes[j] = False
+
+    count_primes[i] = count
+
 class Solution:
     def countPrimes(self, n: int) -> int:
-        def sieveOfEratosthenes(N):
-            if N>2:
-                sieve = [True]*(N)
-                sieve[0] = False
-                sieve[1] = False
-                for i in range(2, int(N**0.5) + 1):
-                    if sieve[i]:
-                        for m in range(i*i, N, i):
-                            sieve[m] = False
-                res = []
-                for idx, number in enumerate(sieve):
-                    if number: 
-                        res.append(idx)
-                return len(res)
+        if n <= 2:
             return 0
-        
-        return (sieveOfEratosthenes(n))
+
+        return count_primes[n-1]
