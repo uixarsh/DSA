1e9aa8ddee8d6fe922a1e8bce3f91a55157c2e2d
Arsh Singhal <130887211+uixarsh@users.noreply.github.com>
2025-05-27 21:52:13 +0530

    Time: 2576 ms (19.02%), Space: 20.6 MB (99.81%) - LeetHub

--- 0204-count-primes/0204-count-primes.py
+++ 0204-count-primes/0204-count-primes.py
@@ -1,22 +1,25 @@
-max_n = int(5 * 1e6)
-
-primes = [True for _ in range(max_n + 1)]
-primes[0], primes[1] = False, False
-count_primes = [0 for _ in range(max_n + 1)]
-
-count = 0
-for i in range(2, max_n + 1):
-    if primes[i]:
-        count += 1
-        
-        for j in range(i * i, max_n + 1, i):
-            primes[j] = False
-
-    count_primes[i] = count
-
 class Solution:
-    def countPrimes(self, n: int) -> int:
-        if n <= 2:
+    def countPrimes(self, limit: int) -> int:
+        if limit < 3:
             return 0
+        if limit == 3:
+            return 1
 
-        return count_primes[n-1]
+        notPrime = [0 for _ in range(((limit >> 1) + 7) >> 3)]
+        count = 2
+        for n in range(5, limit, 6):
+            if not (notPrime[n >> 4] & (1 << ((n >> 1) & 7))):
+                count += 1
+                for x in range(n * n, limit, n + n):
+                    notPrime[x >> 4] |= 1 << ((x >> 1) & 7)
+
+            nPlus2 = n + 2
+            if nPlus2 >= limit:
+                break
+
+            if not (notPrime[nPlus2 >> 4] & (1 << ((nPlus2 >> 1) & 7))):
+                count += 1
+                for x in range(nPlus2 * nPlus2, limit, nPlus2 + nPlus2):
+                    notPrime[x >> 4] |= 1 << ((x >> 1) & 7)
+
+        return count
