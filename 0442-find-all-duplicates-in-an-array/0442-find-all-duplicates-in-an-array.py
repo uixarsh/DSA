57c76622c2ccddd510e4847ba0bc2e6fc9013235
Arsh Singhal <130887211+uixarsh@users.noreply.github.com>
2025-07-01 07:48:56 +0530

    Time: 40 ms (42.9%), Space: 27.1 MB (64.99%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.py
@@ -3,11 +3,16 @@ class Solution:
         res = []
 
         for n in nums:
+            # Really don't care if the num is already marked negative.
             n = abs(n)
 
+            # If num is already marked as negative then we are certain the num repeats.
+            # constraints: num repeats twice only
+            # [1, n]
             if nums[n-1] < 0:
                 res.append(n)
 
+            # Track that the particular num has been checked by updating that ele against the number index.
             nums[n-1] = -nums[n-1]
 
         return res
