98fad087a8c5905e153dbee7d95c99cf60cc0790
Arsh Singhal <130887211+uixarsh@users.noreply.github.com>
2025-07-10 08:14:23 +0530

    Time: 0 ms (100%), Space: 18 MB (69.25%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,23 +1,24 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         close = openn = n
-        op = ""
+        op = []
         rslt = []
         self.solve(openn, close, op, rslt)
         return rslt
 
     def solve(self, openn, close, op, rslt):
         if openn == 0 and close == 0:
-            rslt.append(op)
+            rslt_str = "".join(op)
+            rslt.append(rslt_str)
             return
         
-        if openn !=0:
-            temp = op
-            temp+='('
-            self.solve(openn-1, close,temp, rslt)
+        if openn != 0:
+            op.append('(')
+            self.solve(openn-1, close, op, rslt)
+            op.pop()
         
         if close > openn:
-            temp = op
-            temp+=')'
-            self.solve(openn, close-1, temp, rslt)
+            op.append(')')
+            self.solve(openn, close-1, op, rslt)
+            op.pop()
 
