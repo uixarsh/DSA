373a7cf14798757ed0e9c12569cafc5bcb240bf4
Arsh Singhal <130887211+uixarsh@users.noreply.github.com>
2025-08-27 10:08:39 +0530

    Time: 7 ms (87.06%), Space: 19.2 MB (93.27%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,30 +1,24 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
         n = len(height)
+        lMax = 0
+        rMax = 0
         total = 0
+        l = 0
+        r = n-1
 
-        def prefixMax() -> list:
-            prefix_max = [-1]*n
-            prefix_max[0] = height[0]
-            for i in range(1,n):
-                prefix_max[i] = max(prefix_max[i-1], height[i])
-            return prefix_max
-
-        def suffixMax() -> list:
-            suffix_max = [-1]*n
-            suffix_max[n-1] = height[n-1]
-            for i in range(n-2, -1, -1):
-                suffix_max[i] = max(suffix_max[i+1], height[i])
-            return suffix_max
-
-        left_max = prefixMax()
-        rgt_max = suffixMax()
-
-        for i in range(n):
-            l_max = left_max[i]
-            r_max = rgt_max[i]
-
-            if height[i] < r_max and height[i] < l_max:
-                total+= min(l_max, r_max) - height[i]
-
+        while l<r:
+            if height[l] <= height[r]:
+                if lMax > height[l]:
+                    total += (lMax - height[l])
+                else:
+                    lMax = height[l]
+                l+=1
+            else:
+                if rMax > height[r]:
+                    total += (rMax - height[r])
+                else:
+                    rMax = height[r]
+                r-=1
+        
         return total
