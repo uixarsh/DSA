38ca27d7a877bebaeb78930d9b7c60d4e4b8216f
Arsh Singhal <130887211+uixarsh@users.noreply.github.com>
2025-10-25 10:48:09 +0530

    Time: 0 ms (100%), Space: 18.2 MB (40.98%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -1,21 +1,40 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        start = 0
-        end = len(nums)-1
-        while start<=end:
-            mid = start + (end-start)//2
-            if nums[mid] == target:
-                return mid
-            
-            if nums[start] <= nums[mid]:
-                if nums[start] <= target and target <= nums[mid]:
+        n = len(nums)
+
+        def findMinIdx ():
+            start = 0
+            end = (n-1)
+
+            while start<=end:
+                mid = start + (end-start)//2
+                prev = (mid+n-1)%n
+                nex = (mid+1)%n
+
+                # If already sorted
+                if nums[start] <= nums[end]:
+                    return start
+
+                if nums[mid] <= nums[prev] and nums[mid] <= nums[nex]:
+                    return mid
+
+                # Find the Unsorted Array now
+                if nums[start] <= nums[mid]:
+                    start = mid+1
+                
+                elif nums[mid] <= nums[end]:
+                    end = mid-1
+
+        def bs (start, end):
+            while start <= end:
+                mid = start + (end-start)//2
+                if nums[mid] == target:
+                    return mid
+                elif nums[mid] > target:
                     end = mid-1
                 else:
                     start = mid+1
-            
-            else:
-                if nums[mid] <= target and target <= nums[end]:
-                    start = mid+1
-                else:
-                    end = mid-1
-        return -1
+            return -1
+
+        min_ele = findMinIdx()
+        return max(bs(0,min_ele-1), bs(min_ele, (n-1)))
