1d4be4db71da46a3408dbf673a4c34a07d7530f2
Arsh Singhal <130887211+uixarsh@users.noreply.github.com>
2026-03-06 11:40:18 +0530

    Time: 8 ms (50.15%), Space: 19.4 MB (96.77%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.py
@@ -3,36 +3,27 @@ class Solution:
         # SUM
         # 1 to n2 ka sum
         # 1+2+3+4 = 10
-        ''' 
-        1 3
-        2 4
-
-
-        9 1 7
-        8 9 2
-        3 4 6
-
-        map = {1:, 2: 3: ......}
-
-        mpp = {0: key, 1: key, 2: key}
-        '''
 
         mpp = {}
         n = len(grid)
-        for i in range(1, int(n**2)+1):
-            mpp[i] = 0
 
         for i in range(n):
             for j in range(n):
-                mpp[grid[i][j]]+=1
+                if grid[i][j] not in mpp:
+                    mpp[grid[i][j]] = 1
+                else:
+                    mpp[grid[i][j]] += 1
 
         rslt = [0, 0]
+        n = n**2
+        n_sum = (n)*(n+1) // 2
+        found_sum = 0
 
         for k, v in mpp.items():
             if v==2:
                 rslt[0] = k
-            if v==0:
-                rslt[1] = k
+            found_sum+=k
 
+        rslt[1] = (n_sum-found_sum)
         return rslt
 
