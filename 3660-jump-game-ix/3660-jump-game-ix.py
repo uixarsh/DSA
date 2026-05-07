c93b437852259e957b3228eb17cb404a0fb4b45a
Arsh Singhal <130887211+uixarsh@users.noreply.github.com>
2026-05-07 08:37:30 +0530

    Time: 259 ms (45.69%), Space: 41.1 MB (29.31%) - LeetHub

--- 3660-jump-game-ix/3660-jump-game-ix.py
+++ 3660-jump-game-ix/3660-jump-game-ix.py
@@ -1,31 +1,24 @@
 class Solution:
     def maxValue(self, nums: List[int]) -> List[int]:
         n = len(nums)
-
         ans = [0] * n
-        # [value, index]
-        prev_max = [(0, 0)] * n
+        # [value, left, right]
+        stack = []
 
-        prev = (-math.inf, -1)
-        for i, value in enumerate(nums):
-            if value > prev[0]:
-                prev = (value, i)
-            prev_max[i] = prev
+        for i in range(n):
+            curr_val = nums[i]
+            curr_left = i
+            curr_right = i
 
-        def process(r: int, right_min: float, right_max: float) -> None:
-            p_max, pivot_index = prev_max[r]
-            curr_max = p_max if p_max <= right_min else right_max
+            while stack and stack[-1][0] > nums[i]:
+                top_val, top_left, top_right = stack.pop()
+                curr_val = max(curr_val, top_val)
+                curr_left = top_left
 
-            next_right_min = min(p_max, right_min)
-            for i in range(pivot_index, r + 1):
-                ans[i] = curr_max
-                next_right_min = min(next_right_min, nums[i])
+            stack.append((curr_val, curr_left, curr_right))
 
-            if pivot_index == 0:
-                return
-
-            process(pivot_index - 1, next_right_min, curr_max)
-
-        process(n - 1, math.inf, 0)
+        for i in range(len(stack)):
+            for j in range(stack[i][1], stack[i][2] + 1):
+                ans[j] = stack[i][0]
 
         return ans
