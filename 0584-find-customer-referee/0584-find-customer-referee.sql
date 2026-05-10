0da8e568a38fbe00ce69917c60b7d4e91799dcac
Arsh Singhal <130887211+uixarsh@users.noreply.github.com>
2026-05-10 09:23:25 +0530

    Time: 491 ms (55.35%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,7 +1,4 @@
-# Write your MySQL query statement below
-SELECT
-    name 
-FROM 
-    Customer
-WHERE
-    referee_id != 2 OR referee_id IS NULL;
+/* Write your T-SQL query statement below */
+SELECT name
+FROM Customer
+WHERE referee_id IS NULL OR referee_id != 2;
