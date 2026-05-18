63bbcff893f82bdb71ed2fc1ec2b3cab14244f27
Arsh Singhal <130887211+uixarsh@users.noreply.github.com>
2026-05-18 22:03:49 +0530

    Time: 1621 ms (5.03%), Space: 0B (100%) - LeetHub

---
+++ 0180-consecutive-numbers/0180-consecutive-numbers.sql
@@ -0,0 +1,8 @@
+/* Write your T-SQL query statement below */
+SELECT DISTINCT l1.num AS ConsecutiveNums
+FROM Logs l1
+INNER JOIN Logs l2
+ON l1.id = l2.id-1
+INNER JOIN Logs l3
+ON l2.id = l3.id-1
+WHERE l1.num = l2.num AND l2.num = l3.num;
