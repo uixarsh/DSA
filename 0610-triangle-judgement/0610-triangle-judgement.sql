9f5544a68291112d86d8f4db6598775c9bcacc39
Arsh Singhal <130887211+uixarsh@users.noreply.github.com>
2026-05-18 21:52:16 +0530

    Time: 269 ms (77.85%), Space: 0B (100%) - LeetHub

---
+++ 0610-triangle-judgement/0610-triangle-judgement.sql
@@ -0,0 +1,12 @@
+/* Write your T-SQL query statement below */
+/* Write your T-SQL query statement below */
+select
+    x,
+    y,
+    z,
+    case 
+        when x + y > z and x + z > y and y + z > x then 'Yes'
+        else 'No'
+    end [triangle]
+from
+    Triangle
