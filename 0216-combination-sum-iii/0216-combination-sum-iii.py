54a6a1855f63ffa49fbb07e4cbefbf27793d6004
Arsh Singhal <130887211+uixarsh@users.noreply.github.com>
2026-05-28 13:54:32 +0530

    Time: 0 ms (100%), Space: 19.4 MB (34.16%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,9 +1,6 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
 
-        if k>n:
-            return []
-
         def solve(k, curr, n, rslt, prev_sum, pair):           
             if not k:
                 if prev_sum == n:
@@ -12,6 +9,8 @@ class Solution:
             
             for i in range(curr, 10):
                 curr_sum = i + prev_sum
+                if curr_sum > n:
+                    break
                 pair.append(i)
                 solve (k-1, i+1, n, rslt, curr_sum, pair)
                 pair.pop()
