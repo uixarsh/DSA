7a7a606e3436468b9d80d41223f8e3c922626323
Arsh Singhal <130887211+uixarsh@users.noreply.github.com>
2026-05-28 13:50:54 +0530

    Time: 3 ms (21.03%), Space: 19.3 MB (73.34%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.py
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -1,20 +1,21 @@
 class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
-        res = []
-        path = []
-        self.get_combinations(0, path, res, n, k)
-        return res
 
-    def get_combinations(self, start : int, path : List[int], res : List[List[int]], n : int, k : int):
-        
-        # Base Condition
-        if k==0:
-            if sum(path) == n:
-                res.append(path[:])
-            return
-        
-        # Control Statement
-        for i in range(start+1, 10):
-            path.append(i)
-            self.get_combinations(i, path, res, n, k-1)
-            path.pop()
+        if k>n:
+            return []
+
+        def solve(k, curr, n, rslt, prev_sum, pair):           
+            if not k:
+                if prev_sum == n:
+                    rslt.append(pair[:])
+                return
+            
+            for i in range(curr, 10):
+                curr_sum = i + prev_sum
+                pair.append(i)
+                solve (k-1, i+1, n, rslt, curr_sum, pair)
+                pair.pop()
+
+        rslt = []
+        solve(k, 1,  n, rslt, 0, [])
+        return rslt
