4d17ea7a0aae0c0cbddbf9185cae9dd09ccacb50
Arsh Singhal <130887211+uixarsh@users.noreply.github.com>
2026-06-01 09:23:52 +0530

    Time: 0 ms (100%), Space: 19.5 MB (86.75%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,26 +1,26 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-
         nums.sort()
+
 
         def solve(nums, n, rslt, curr):
             if n == 0:
                 rslt.append(curr[:])
                 return
-
+            
             for i in range(n):
-                ele = nums[i]
+
+                if i > 0 and nums[i] - nums[i-1] == 0:
+                    continue
+                
+                ele = nums.pop(i)
                 curr.append(ele)
-                nums.pop(i)
+                
                 solve(nums, n-1, rslt, curr)
+
                 nums.insert(i, ele)
                 curr.pop()
-
+        
         rslt = []
         solve(nums, len(nums), rslt, [])
-
-        final = []
-        for ele in rslt:
-            if ele not in final:
-                final.append(ele)
-        return final
+        return rslt
