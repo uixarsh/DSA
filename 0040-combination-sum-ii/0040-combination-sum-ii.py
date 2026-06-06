73a8e3fce24893eb07f9290ae78df060ffa929a6
Arsh Singhal <130887211+uixarsh@users.noreply.github.com>
2026-06-06 11:09:17 +0530

    Time: 3 ms (87.03%), Space: 19.9 MB (13.22%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -1,20 +1,19 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int):
 
-        candidates.sort()
+        candidates.sort()   # nlog(n)
         n = len(candidates)
 
         def solve(idx, summ, curr):
 
             if summ == target:
                 rslt.append(curr[:])
-                return True
+                return
 
             if idx >= n or summ > target:
-                return False
+                return
             
             ele = candidates[idx]
-            
             if ele > target - summ:
                 return
 
@@ -33,3 +32,34 @@ class Solution:
         rslt = []
         solve(0, 0, [])
         return rslt
+
+
+
+'''     
+  [10,1,2,7,6,1,5]
+  [1, 1, 2, 5, 6, 7]
+
+                                        f(idx, summ, curr)
+                                        f(0, 0, [])
+                                    /                   \
+                        f(1, 0, [])                                 f(1, 1, [1])
+                            /    \                                     / \
+                f(2, 0, [])     f(2, 1, [1])            f(2, 1, [1])       f(2, 2, [1, 1])   
+                /    \              /   \ 
+f(3, 0, [])     f(3, 1, [1])     f(3, 1, [1])   f(3, 2, [1, 1])
+
+
+            if ele > target - summ:
+                return
+
+            8 - 4 = 4 5 > 4
+len(arr)
+
+2 choices n branches
+2^n
+
+2^7 = 128
+
+
+
+'''
